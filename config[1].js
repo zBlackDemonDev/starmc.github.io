dea1b15d477e3d4821e65e1b20e48e3e38ba58df
BlackDemonDev <145194644+zBlackDemonDev@users.noreply.github.com>
2025-09-07 00:53:19 +0200

    Add files via upload

---
+++ config[1].js
@@ -0,0 +1,7 @@
+// Configurable links
+const config = {
+  title: "Minecraft Server | Maintenance",
+  discord: "https://discord.gg/yourserver",
+  vote: "https://minecraft-server-vote-link.com",
+  store: "https://yourstore.com"
+};
